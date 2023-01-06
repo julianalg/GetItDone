1fce22065f6303028f1a07f3905a7cba6bf07d4f
Julian Garcia <jelogarcia2008@gmail.com>
2023-01-05 18:07:05 -0800

    updated to do list functionality, added ability to save tasks to an array, have them print out

--- testToDolist-Andy/ToDoList.js
+++ testToDolist-Andy/ToDoList.js
@@ -1,59 +1,44 @@
 console.log("Javascript started");
 
-const tasks = new Array();
-
+let tasks = []
 console.log(`Tasks: ${tasks}`);
 
+
 function addItem() {
-
-    console.log("Add button has been pressed");
-
-    const task  = document.createElement("li");
     
-    const taskNumber = tasks.length;
-
-    console.log("Task element created");
-
+    let ul = document.getElementById("tasklist")
+    console.log("Add button has been pressed");
+    const task  = document.createElement("li");
+    console.log("Task element created")
     task.innerText = document.getElementById("taskContent").value;
-
-    task.id = task.innerText;
-
-    console.log(`Task ID: ${task.id}`)
-
-    console.log(`Task text: ${task.innerText}`)
-
-    console.log("Task text changed");
-
-    tasks.push(task);
-
-    console.log("Task added to list");
-
     console.log(`Tasks: ${tasks}`)
-
-    chrome.storage.local.set({taskNumber: task});
-
-    document.querySelector("ul").append(...tasks);
-
+    const SubmitTask = task.innerHTML
+    tasks.push(SubmitTask)
+    console.log(tasks)
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(task.innerText));
+    ul.appendChild(li);
     console.log("List appended to document");
-
 } 
 
 function removeItem() {
-
+    
+    tasks = []
+    
     console.log("Remove button has been pressed");
-
+    
     console.log(`List of Tasks: ${tasks}`);
-
+    
 }
 
 function testStorage() {
-
+    
     const element = chrome.storage.local.get("1");
-
+    
     console.log(element);
-
+    
     console.log(element.innerText);
-
+    
 }
 
 const addTaskButton = document.getElementById("addTask");
