c357dca71ad991eb802fb5991d6799a33880eec4
rake6682 <andy.t.ly3@gmail.com>
2023-01-10 18:48:42 -0800

    Added task completion and deletion

--- testToDolist-Andy/ToDoList.js
+++ testToDolist-Andy/ToDoList.js
@@ -2,7 +2,7 @@ console.log("Javascript started");
 
 let taskList = [];
 
-chrome.storage.sync.get(["taskList"], function(result) {
+chrome.storage.sync.get(["taskList"], (result) => {
     
     if (result.taskList) {
         
@@ -25,29 +25,110 @@ function displayTaskList() {
     
     const taskListElement = document.getElementById("taskList");
 
-    taskListElement.innerHTML = ""; //clear rather than append because append logic gets messy fast
+    const completedTasksElement = document.getElementById("completedTasks");
 
-    for(let i = 0; i < taskList.length; i++) {
+    taskListElement.innerHTML = ""; //clear rather than append because append logic gets messy fast.
+
+    completedTasksElement.innerHTML = "";
+
+    // Below code actually starts rendering
+
+    for (let i = 0; i < taskList.length; i++) {
 
         const task = taskList[i];
 
         const taskElement = document.createElement("li");
 
-        taskElement.classList.add("Incomplete"); //Marks as complete or incomplete for later styling
+        //Marks as complete or incomplete for later
 
         if (task.completed == true) {
 
             taskElement.classList.add("Completed");
 
+        } else {
+
+            taskElement.classList.add("Incomplete");
+
         }
 
         taskElement.innerText = task.text;
 
-        taskListElement.appendChild(taskElement);
+        const removeButton = document.createElement("button");
 
+        removeButton.innerText = "Remove";
+
+        removeButton.addEventListener("click", () => {
+
+            removeTask(i);
+
+            displayTaskList(); //Display task here rather than in the function because the functions should only modify taskList, not display it.
+        
+        });
+        
+        const checkbox = document.createElement("input");
+
+        checkbox.type = "checkbox";
+
+        checkbox.checked = task.completed; // If task is not completed, it will be unchecked. If task is completed, it will be checked.
+
+        checkbox.addEventListener("click", () => {
+            
+            toggleTask(i); //Saves what happened a few lines above to the taskList
+            
+            displayTaskList();
+        
+        });
+
+        taskElement.appendChild(removeButton);
+
+        taskElement.appendChild(checkbox);
+
+        if (taskElement.className == "Completed") {
+
+            completedTasksElement.appendChild(taskElement);
+
+        } else {
+
+            taskListElement.appendChild(taskElement);
+
+        }
     }
 
 }
+
+function addTask(text) {
+
+    console.log("Task being added...");
+
+    taskList.push({
+
+        text: text,
+
+        completed: false
+
+    });
+
+    storeList();
+
+}
+
+function removeTask(index) {
+
+    console.log("Task being removed...");
+
+    taskList.splice(index, 1);
+
+    storeList();
+
+}
+
+function toggleTask(index) {
+
+    taskList[index].completed = !taskList[index].completed;
+
+    storeList();
+
+  }
 
 function storeList() {
 
@@ -65,25 +146,9 @@ function storeList() {
 
 }
 
-function addTask(text) {
-
-    console.log("Task being added");
-
-    taskList.push({
-
-        text: text,
-
-        completed: false
-
-    });
-
-    storeList();
-
-} 
-
 const addTaskForm = document.getElementById("addTaskForm"); 
 
-addTaskForm.addEventListener("submit", function(event) {
+addTaskForm.addEventListener("submit", (event) => {
 
     event.preventDefault() //So we can do things with the info in the form
 
