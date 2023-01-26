6f6198ebcb3c4500ed25a75250955ec304f94677
rake6682 <andy.t.ly3@gmail.com>
2023-01-25 19:12:23 -0800

    mucked around with blocking.js, hp is still not properly synced and displayed

--- testToDolist-Andy/blocking/blocking.js
+++ testToDolist-Andy/blocking/blocking.js
@@ -1,1 +1,26 @@
 console.log("Loading blocking.js");
+
+let user = {};
+
+chrome.storage.sync.get(["user"], (result) => {
+    
+    if (result.user) {
+        
+        user = result.user;
+    } 
+    
+    else {
+        
+        chrome.storage.sync.set({"user": {hp: 0, level: 0}});
+        
+    }
+
+});
+
+user.hp = user.hp-5;
+
+chrome.storage.sync.set({"user": {hp: 0}});
+
+const hpDisplay = document.getElementById("HP");
+
+hpDisplay.innerText = user.hp;
