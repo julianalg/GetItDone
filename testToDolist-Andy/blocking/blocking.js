53266631ac9e1e41a24dedd86f14d3ecd6586698
rake6682 <andy.t.ly3@gmail.com>
2023-01-28 09:09:56 -0800

    stylized blocking.js and added helpful functions to focusGame.js

--- testToDolist-Andy/blocking/blocking.js
+++ testToDolist-Andy/blocking/blocking.js
@@ -1,33 +1,44 @@
 console.log("Loading blocking.js");
 const hpDisplay = document.getElementById("HP");
-const workButton = document.getElementById("backToWork")
-const bypassButton = document.getElementById("bypass")
-let userHP = document.getElementById("backToWork")
+const workButton = document.getElementById("backToWork");
+const bypassButton = document.getElementById("bypass");
+let userHP = document.getElementById("backToWork");
 
 chrome.storage.sync.get(["user"], (result) => {
-    hpDisplay.innerText = result.user.hp - 5
-    chrome.storage.sync.set({"user": {hp: hpDisplay.innerText, level: 0}})
 
-})
+    hpDisplay.innerText = result.user.hp - 5;
+
+    chrome.storage.sync.set({"user": {hp: hpDisplay.innerText, level: 0}});
+
+});
 
 chrome.storage.sync.get(["user"], (result) => {
-    console.log(result.user.hp)
-})
+    
+    console.log(result.user.hp);
+
+});
 
 workButton.addEventListener("click", function() {
-    window.close()
-})
+
+    window.close();
+
+});
 
 bypassButton.addEventListener("click", function() {
+
     chrome.storage.sync.get(["user"], (result) => {
-        console.log(result.user.hp)
-        console.log(result.user.hp - 20)
-        chrome.storage.sync.set({"user": {hp: result.user.hp - 20, level: 0}})
-    })
+
+        console.log(result.user.hp);
+
+        console.log(result.user.hp - 20);
+
+        chrome.storage.sync.set({"user": {hp: result.user.hp - 20, level: 0}});
+
+    });
 
     // i need to figure out how to get the URL that the user was attempting to go to 
 
-})
+});
 
 
 
--- testToDolist-Andy/focusGame.js
+++ testToDolist-Andy/focusGame.js
@@ -1,45 +1,50 @@
 console.log("Loading focusGame.js");
 
-let hp;
+let localHp;
 let level; 
-let body = document.getElementById("body")
+let body = document.getElementById("body");
 const hpReadout = document.getElementById('hp-readout');
 const hpTest = document.getElementById('hp-test');
+
+function displayHP() {
+
+    const hpReadout = document.getElementById('hp-readout');
+
+    hpReadout.innerText = localHp;
+
+}
+
+function storeHP() {
+
+    chrome.storage.sync.set({"user": {hp: localHp}});
+
+}
 
 body.addEventListener("load", function() {
 
     chrome.storage.sync.get(["user"], (result) => {
 
-        console.log(result.user.hp)
+        console.log(result.user.hp);
 
-        hpReadout.innerHTML = result.user.hp
+        hp = result.user.hp;
 
     });
+
+    displayHP();
+
 });
 
 hpTest.addEventListener("click", function() {
     
-    chrome.storage.sync.set({"user": {hp: 69}}, () => {
+    hp += 5;
 
-        if (chrome.runtime.lastError) {
+    storeHP();
 
-            console.log('Error setting' + chrome.runtime.lastError);
-
-        }
-        
-    });
-
-    chrome.storage.sync.get(["user"], (result) => {
-
-        console.log(result.user.hp);
-
-        hpReadout.innerHTML = "HP: " + result.user.hp;
-
-    });
+    displayHP();
 
 });
 
-
+/* Im pretty sure the below code makes it so user hp is always 0
 chrome.storage.sync.set({"user": {
     
     hp: 0, 
@@ -54,4 +59,4 @@ chrome.storage.sync.get(["user"], (result) => {
 
     hpReadout.innerHTML = "HP: " + result.user.hp;
 
-}) 
+}) */
