74cabd9e2cb1a8520616390bc8fd0a8734f08524
rake6682 <andy.t.ly3@gmail.com>
2023-01-28 09:42:08 -0800

    cant check whether or not user obj is null/undefined/{} so i cant properly load it

--- testToDolist-Andy/focusGame.js
+++ testToDolist-Andy/focusGame.js
@@ -1,7 +1,6 @@
 console.log("Loading focusGame.js");
 
-let localHp;
-let level; 
+let user;
 let body = document.getElementById("body");
 const hpReadout = document.getElementById('hp-readout');
 const hpTest = document.getElementById('hp-test');
@@ -10,13 +9,13 @@ function displayHP() {
 
     const hpReadout = document.getElementById('hp-readout');
 
-    hpReadout.innerText = localHp;
+    hpReadout.innerText = user.hp;
 
 }
 
 function storeHP() {
 
-    chrome.storage.sync.set({"user": {hp: localHp}});
+    chrome.storage.sync.set({"user": {hp: user.hp}});
 
 }
 
@@ -24,9 +23,19 @@ body.addEventListener("load", function() {
 
     chrome.storage.sync.get(["user"], (result) => {
 
-        console.log(result.user.hp);
+        if (result.user && Object.keys(obj).length === 0 && Object.getPrototypeOf(result.user) === Object.prototype) { 
 
-        localHp = result.user.hp;
+            console.log(result.user.hp);
+
+            user = result.user;
+
+        } else {
+
+            chrome.storage.sync.set({"user": {hp: 0, level: 0}});
+
+            user = {hp: 0, level: 0}; 
+
+        }
 
     });
 
@@ -36,7 +45,7 @@ body.addEventListener("load", function() {
 
 hpTest.addEventListener("click", function() {
     
-    localHp += 5;
+    user.hp += 5;
 
     storeHP();
 
